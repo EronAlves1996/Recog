b5c08c752a4ef186f0325df928df7331a0e6ab4c
Eron Alves <eron.alves@rocketmail.com>
2025-11-09 22:16:42 -0300

    refactor: move entire app to internal package

---
+++ internal/app/init.go
@@ -0,0 +1,36 @@
+package app
+
+import (
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+var errInternalServerError = errors.New("internal server error")
+
+func Run() {
+	config, err := LoadConfig()
+	if err != nil {
+		log.Fatal(fmt.Errorf("unable to load app config: %w", err))
+	}
+	rsaPair, err := ParseRsaPair(config.RawRsaPrivateKey)
+	if err != nil {
+		log.Fatal(fmt.Errorf("unable to parse rsa pair: %w", err))
+	}
+
+	router := gin.Default()
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(fmt.Errorf("unable to create logger: %w", err))
+	}
+
+	l := logger.Sugar()
+	router.MaxMultipartMemory = 8 << 20
+	registerRoutes(l, rsaPair, router)
+
+	l.Info("Listening on 8080")
+	router.Run()
+}
--- main.go
+++ main.go
@@ -1,36 +1,9 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"log"
-
-	"github.com/gin-gonic/gin"
-	"go.uber.org/zap"
+	"github.com/EronAlves1996/Recog/internal/app"
 )
 
-var errInternalServerError = errors.New("internal server error")
-
 func main() {
-	config, err := LoadConfig()
-	if err != nil {
-		log.Fatal(fmt.Errorf("unable to load app config: %w", err))
-	}
-	rsaPair, err := ParseRsaPair(config.RawRsaPrivateKey)
-	if err != nil {
-		log.Fatal(fmt.Errorf("unable to parse rsa pair: %w", err))
-	}
-
-	router := gin.Default()
-	logger, err := zap.NewProduction()
-	if err != nil {
-		log.Fatal(fmt.Errorf("unable to create logger: %w", err))
-	}
-
-	l := logger.Sugar()
-	router.MaxMultipartMemory = 8 << 20
-	registerRoutes(l, rsaPair, router)
-
-	l.Info("Listening on 8080")
-	router.Run()
+	app.Run()
 }
