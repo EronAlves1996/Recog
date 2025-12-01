8c0c6607c92b1adcee6b0903c5a280cec9d09d13
Eron Alves <eron.alves@rocketmail.com>
2025-12-01 00:01:38 -0300

    Squashed commit of the following:
    commit 52df1e74920cfd3e3aafd7c0902883515dabf575
    Author: Eron Alves <eron.alves@rocketmail.com>
    Date:   Mon Dec 1 00:00:59 2025 -0300
    
        chore: update readme
    
    commit 102ba573097c075aaa64feb8393788cf666098ad
    Author: Eron Alves <eron.alves@rocketmail.com>
    Date:   Sun Nov 30 23:58:18 2025 -0300
    
        fix: accepted minimum key sizes
    
    commit bb2d4d3b237d5fee81792dd4ab6195d280ea33d7
    Author: Eron Alves <eron.alves@rocketmail.com>
    Date:   Sun Nov 30 23:53:56 2025 -0300
    
        feat: implement more validations
    
    commit 99a2cc2eb0bf34602ecd6d5d1c902a95baa40d61
    Author: Eron Alves <eron.alves@rocketmail.com>
    Date:   Sun Nov 30 23:39:01 2025 -0300
    
        feat: implement ocsp revogation verification
    
    commit 6c6ab3f7add5b56ebfe397199ccf8595c45b7b81
    Author: Eron Alves <eron.alves@rocketmail.com>
    Date:   Sun Nov 30 23:21:32 2025 -0300
    
        chore: chain validation explanation

--- internal/app/certificate/validate.go
+++ internal/app/certificate/validate.go
@@ -1,13 +1,21 @@
 package certificate
 
 import (
+	"bytes"
 	"context"
+	"crypto"
+	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
+	"slices"
 
 	"github.com/EronAlves1996/Recog/internal/app/base"
+	"golang.org/x/crypto/ocsp"
 )
 
 type validateCertificateAction struct{}
@@ -39,6 +47,7 @@ func (v validateCertificateAction) Execute(ctx context.Context, in *[]byte) (*bo
 	caCertPool.AppendCertsFromPEM(rootCa)
 
 	verifyOptions := x509.VerifyOptions{
+		// Already validate the entire chain
 		Roots: caCertPool,
 	}
 
@@ -47,6 +56,124 @@ func (v validateCertificateAction) Execute(ctx context.Context, in *[]byte) (*bo
 		return nil, fmt.Errorf("unable to verify certificate chain: %w", err)
 	}
 
-	valid := len(chains) > 0
-	return &valid, nil
+	if len(chains) <= 0 {
+		v := false
+		return &v, nil
+	}
+
+	ocspValid, err := validateOcspRevogation(chains)
+	if err != nil {
+		return nil, fmt.Errorf("unable to verify ocsp revogation: %w", err)
+	}
+
+	if !ocspValid {
+		v := false
+		return &v, nil
+	}
+
+	if !validateCertificateSignatureAlgorithm(certificate) {
+		v := false
+		return &v, nil
+	}
+
+	parsed, ok := certificate.PublicKey.(*rsa.PublicKey)
+	if !ok {
+		return nil, fmt.Errorf("unable to parse public key")
+	}
+
+	if parsed.Size() < 2048 {
+		v := false
+		return &v, nil
+	}
+
+	l := true
+	return &l, nil
+}
+
+var validSignatureAlgorithms = []x509.SignatureAlgorithm{
+	x509.SHA256WithRSA,
+	x509.SHA384WithRSA,
+	x509.SHA512WithRSA,
+}
+
+func validateCertificateSignatureAlgorithm(certificate *x509.Certificate) bool {
+	return slices.Contains(validSignatureAlgorithms, certificate.SignatureAlgorithm)
+}
+
+func validateOcspCertificatePair(certificate, issuer *x509.Certificate) (bool, error) {
+	opts := ocsp.RequestOptions{
+		Hash: crypto.SHA256,
+	}
+
+	if len(certificate.OCSPServer) == 0 {
+		return true, nil
+	}
+
+	if certificate.OCSPServer[0] == "" {
+		return true, nil
+	}
+
+	buffer, err := ocsp.CreateRequest(certificate, issuer, &opts)
+	if err != nil {
+		return false, fmt.Errorf("unable to create ocsp revogation request: %w", err)
+	}
+
+	httpRequest, err := http.NewRequest(http.MethodPost, certificate.OCSPServer[0], bytes.NewBuffer(buffer))
+	if err != nil {
+		return false, err
+	}
+
+	ocspURL, err := url.Parse(certificate.OCSPServer[0])
+	if err != nil {
+		return false, err
+	}
+	httpRequest.Header.Add("Content-Type", "application/ocsp-request")
+	httpRequest.Header.Add("Accept", "application/ocsp-response")
+	httpRequest.Header.Add("host", ocspURL.Host)
+
+	httpClient := &http.Client{}
+	httpResponse, err := httpClient.Do(httpRequest)
+	if err != nil {
+		return false, err
+	}
+	defer httpResponse.Body.Close()
+	output, err := io.ReadAll(httpResponse.Body)
+
+	if err != nil {
+		return false, err
+	}
+
+	ocspResponse, err := ocsp.ParseResponseForCert(output, certificate, issuer)
+	if err != nil {
+		return false, err
+	}
+
+	if ocspResponse.Status == ocsp.Good {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func validateOcspRevogation(chains [][]*x509.Certificate) (bool, error) {
+
+	for _, v := range chains {
+		chainSize := len(v)
+		for i := range v {
+			if (i + 1) == chainSize {
+				break
+			}
+
+			valid, err := validateOcspCertificatePair(v[i], v[i+1])
+			if err != nil {
+				return false, fmt.Errorf("unable to validate pair: %w", err)
+			}
+
+			if !valid {
+				return false, nil
+			}
+		}
+	}
+
+	return true, nil
 }
