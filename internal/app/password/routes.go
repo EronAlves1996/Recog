89da6277a21c3ba049406161eebce05094566c00
Eron Alves <eron.alves@rocketmail.com>
2025-12-09 22:40:50 -0300

    feat: setup endpoints

--- internal/app/password/routes.go
+++ internal/app/password/routes.go
@@ -1,7 +1,30 @@
 package password
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/EronAlves1996/Recog/internal/app/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+type HashPasswordBody struct {
+	Password string `json:"password" binding:"required"`
+}
+
+type VerifyPasswordBody struct {
+	HashPasswordBody
+	HashedPassword string `json:"hashedPassword" binding:"required"`
+}
 
 func RegisterRoutes(r gin.IRouter) {
 	rg := r.Group("password")
+
+	rg.POST("/hash", middleware.ValidateMiddleware(HashPasswordBody{}), hashPassword())
+	rg.POST("/verify", middleware.ValidateMiddleware(VerifyPasswordBody{}), verifyPassword())
+}
+
+func verifyPassword() gin.HandlerFunc {
+	panic("unimplemented")
+}
+
+func hashPassword() gin.HandlerFunc {
+	panic("unimplemented")
 }
