7bea351f31464e3d7ee15d4d74105ee7edb62941
Eron Alves <eron.alves@rocketmail.com>
2025-12-09 22:48:07 -0300

    feat: hash password

--- internal/app/password/routes.go
+++ internal/app/password/routes.go
@@ -1,13 +1,17 @@
 package password
 
 import (
+	"net/http"
+
+	"github.com/EronAlves1996/Recog/internal/app/httputils"
 	"github.com/EronAlves1996/Recog/internal/app/middleware"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type HashPasswordBody struct {
-	Password string `json:"password" binding:"required"`
+	Password string `json:"password" binding:"required,max=70"`
 }
 
 type VerifyPasswordBody struct {
@@ -28,5 +32,23 @@ func verifyPassword(l *zap.SugaredLogger) gin.HandlerFunc {
 }
 
 func hashPassword(l *zap.SugaredLogger) gin.HandlerFunc {
-	panic("unimplemented")
+	return func(c *gin.Context) {
+		body, ok := c.MustGet(gin.BindKey).(*HashPasswordBody)
+		if !ok {
+			httputils.AbortFailedToDeserialize(l, c)
+			return
+		}
+
+		password := body.Password
+		cipherText, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+		if err != nil {
+			l.Errorw("failed to encrypt password", zap.Error(err))
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"hashedPassword": string(cipherText),
+		})
+	}
 }
